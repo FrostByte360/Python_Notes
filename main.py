8d4ba7ab88a07a2bf607d7f422f01955dabd2d07
FrostByte360 <225485059+FrostByte360@users.noreply.github.com>
2026-03-25 10:07:26 +0800

    Create Operation Documentation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print ("Hello, World!")
 # This is my first Python program.
 
 # VARIABLES
-#=====================================================================================================================
+#=======================================================================================================================
 
 # A variable is a reusable container for value. There are four basic data types:
 # # int, string, float, and char.
@@ -26,10 +26,35 @@ print (f"I am {legal_age} years old.") # PRINTS "I am 21 years old."
 
 #   float      value
 #     v          v
-general_averge = 8.4
-print (f"I have a grade of {general_averge}.") # PRINTS "I have a grade of 8.4."
+general_average = 8.4
+print (f"I have a grade of {general_average}.") # PRINTS "I have a grade of 8.4."
 
 #    bool        value
 #     v            v
 isStillAStudent = True # True and False must need to be capitalized
 print (f"Am I still a student? That is {isStillAStudent}.") # PRINTS "Am I still a student? That is True}."
+
+# BASIC ARITHMETIC
+#=======================================================================================================================
+
+# In basic arithmetic, we have the following operators:
+# +   Addition
+# -   Subtraction
+# *   Multiplication
+# /   Division (returns a float/fractional value)
+# //  Integer Division (returns a whole numbered value)
+# %   Modulus Operator (returns the remainder)
+
+pie_slices = 6
+print(f"I have a total of {pie_slices} pie slices.") # PRINTS "I have a total of 6 pie slices."
+
+pie_slices += 6 # This is an augmented assignment operator. This can be interpreted as pie_slices = pie_slices + 6
+print(f"I now have have a total of {pie_slices} pie slices.") # PRINTS "I now have have a total of 12 pie slices."
+
+pie_slices -= 3
+print(f"My friends ate some slices. I now have have a total of {pie_slices} pie slices.")
+# PRINTS "My friends ate some slices. I now have have a total of 9 pie slices."
+
+pie_slices *= 3
+print(f"Luckily my mom bought some more. Now I have {pie_slices} pie slices.")
+# PRINTS "Luckily my mom bought some more. Now I have 27 pie slices."
